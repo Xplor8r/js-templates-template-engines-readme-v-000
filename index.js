2370d84f1c7c005111ff0a398937748aab7c91e9
Michael Pieschel <mpieschel39@gmail.com>
2018-12-04 13:21:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ function postComment() {
  
   let commentTemplate =
     '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  let templateFn = _.template(commentTemplate);
+  let commentsDiv = document.getElementById('comments');
+  let templateHTML = templateFn({ comment: comment, commenter: commenter });
+  commentsDiv.innerHTML += templateHTML;
 }
