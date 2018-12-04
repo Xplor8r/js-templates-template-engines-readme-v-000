a40db11d33438fe424515e027819fc50f627ed7f
Michael Pieschel <mpieschel39@gmail.com>
2018-12-04 13:21:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 function postComment() {
   let commenter = document.getElementById('commenterName').value;
   let comment = document.getElementById('commentText').value;
-  //insert comment into "comments" div in this format:
-  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
- 
   let commentTemplate =
     '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
   let templateFn = _.template(commentTemplate);
